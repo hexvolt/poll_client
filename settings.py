5988704475188fc5d83d0d16e25dcd019e5ef65a
hexvolt <hexvolt@gmail.com>
2016-04-15 20:08:04 +0300

    ticket:2 logger settings added

---
+++ settings.py
@@ -0,0 +1,40 @@
+import os
+
+# RabbitMQ consumer settings
+RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST', 'localhost')
+RABBITMQ_PORT = os.environ.get('RABBITMQ_PORT', 5672)
+RABBITMQ_USERNAME = os.environ.get('RABBITMQ_USERNAME', 'guest')
+RABBITMQ_PASSWORD = os.environ.get('RABBITMQ_PASSWORD', 'guest')
+RABBITMQ_APP_EXCHANGE = 'poll'
+
+# Logging settings
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+            'stream': 'ext://sys.stdout'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'WARNING',
+            'propagate': 0
+        },
+        'consumer': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': 0
+        }
+    }
+}
