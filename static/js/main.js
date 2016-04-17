29c900b1a672ba60d860ad2f854c291a9c465214
hexvolt <hexvolt@gmail.com>
2016-04-17 09:38:46 +0300

    ticket:4 dynamic update JS handlers

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,50 @@
-function messageProcess(message){
-    console.log(message)
+$(document).ready(function() {
+
+    function updateChoice(id, instance) {
+        var $choice = $("#choice_" + id),
+            $choiceText = $choice.find(".choice-text"),
+            $choiceVotes = $choice.find(".votes");
+
+        $choiceText.text(instance.choice_text);
+        $choiceVotes.text(instance.votes);
+    }
+
+    function updateQuestion(id, instance) {
+        var $question = $("#question_" + id),
+            $questionText = $question.find(".question-text");
+
+        $questionText.text(instance.question_text);
+    }
+
+    function messageProcess(message){
+        var msgJSON = $.parseJSON(message),
+            action = msgJSON.action.toLowerCase(),
+            model = msgJSON.model.toLowerCase(),
+            instance = msgJSON.instance,
+            instanceID = instance.id;
+
+        if ((model === "choice") && (action === "updated")) {
+            updateChoice(instanceID, instance);
+        } else if ((model === "question") && (action === "updated")) {
+            updateQuestion(instanceID, instance);
+        }
+
+        console.log(message)
+    }
+
+    function websocketSubscribePollChanges(){
+
+        var ws = new WebSocket("ws://0.0.0.0:8888/subscribe");
+        ws.onopen = function() {
+            console.log("Connected to WebSocket");
+        };
+        ws.onmessage = function(event) {
+            messageProcess(event.data);
+        };
+        ws.onclose = function() {
+            console.log("WebSocket connection closed");
+        };
 }
+    websocketSubscribePollChanges();
+});
 
-function websocketSubscribePollChanges(){
-
-    var ws = new WebSocket("ws://0.0.0.0:8888/subscribe");
-    ws.onopen = function() {
-        console.log("Connected to WebSocket");
-    };
-    ws.onmessage = function(event) {
-        messageProcess(event.data);
-    };
-    ws.onclose = function() {
-        console.log("WebSocket connection closed");
-    };
-}
-
-websocketSubscribePollChanges();
